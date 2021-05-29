ce11ec53d71612cc09477e4b57198ea019a348ec
jonathanjosef91 <54238904+jonathanjosef91@users.noreply.github.com>
2021-05-29 15:43:24 +0300

    Taase mashehoo

--- u.hpp
+++ u.hpp
@@ -2,21 +2,55 @@
 #define U_HPP
 
 enum TYPE {
+    E_bool,
     E_byte,
     E_int,
     E_string
 };
 
-typedef struct exp_t {
+class Exp_t{
+public:
     TYPE t;
 
-} exp_t;
+    virtual void* getVal() = 0;
+};
 
-typedef struct test_object_t{
+class Integer_t : public Exp_t{
+public:
     int val;
-    char op[100];
-    exp_t exp;
-}test_object;
-#define YYSTYPE test_object
+
+    Integer_t(int _val) : val(_val), t(E_int);
+    Integer_t(char * val) {
+        cout << "Integer_t c'tor" << val << endl;
+    }
+    void* getVal() { return &val; }
+};
+
+class Bool_t : public Exp_t{
+public:
+    bool val;
+
+    Integer_t(bool _val) : val(_val), t(E_int);
+    void* getVal() { return &val; }
+};
+
+class String_t : public Exp_t{
+public:
+    char* val;
+
+    Integer_t(char* _val) : val(_val), t(E_int);
+    void* getVal() { return val; }
+};
+
+typedef struct objects_pool_t{
+    Integer_t integerVal;
+    Bool_t boolVal;
+    String_t stringVal;
+
+    Exp_t expVal;
+
+}objects_pool;
+
+#define YYSTYPE objects_pool
 
 #endif
