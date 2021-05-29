024be6a06e6c2ebef19383109856b92f2f810f61
jonathanjosef91 <54238904+jonathanjosef91@users.noreply.github.com>
2021-05-29 15:16:24 +0300

    Taase mashehoo

--- u.hpp
+++ u.hpp
@@ -1,10 +1,15 @@
 #ifndef U_HPP
 #define U_HPP
 
+typedef struct exp_t {
+    TYPE t;
+
+} exp_t;
+
 typedef struct test_object_t{
     int val;
     char op[100];
-
+    exp_t exp;
 }test_object;
 #define YYSTYPE test_object
 
